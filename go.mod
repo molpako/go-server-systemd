1bec75e0af983e07f7c3a3d41b2bf45a96b0e744
molpako <system.222.down@gmail.com>
2022-12-12 11:53:13 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/molpako/go-server-systemd
+
+go 1.19
+
+require github.com/coreos/go-systemd/v22 v22.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,57 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/coreos/go-systemd/v22/activation"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	now := time.Now()
+	io.WriteString(w, "good afternoon socket activated world!\n")
+	io.WriteString(w, now.GoString())
+}
+
+func main() {
+	listeners, err := activation.Listeners()
+	if err != nil {
+		panic(err)
+	}
+
+	if len(listeners) == 0 {
+		panic("Unexpected number of socket activation fds")
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HelloServer)
+	srv := &http.Server{
+		Handler: mux,
+	}
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, syscall.SIGHUP)
+		s := <-sigint
+		log.Printf("HTTP server Reload: %v", s)
+
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		log.Println("HTTP server Shutdown: OK")
+		close(idleConnsClosed)
+	}()
+	if err := srv.Serve(listeners[0]); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
+}
